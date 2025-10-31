17a82ead41afdd23e1cf1692e810b9d602900961
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:41:45 +0500

    Add files via upload

---
+++ display_datatype.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 8:display all datatype like integer,float,string
+    int age = 20;
+    string name = "hannan amin";
+    float marks = 89.5;
+    cout<<" Stuent name: "<< name << endl;
+    cout<<" Age is: "<< age << endl;
+    cout<<" Student marks: "<< marks;
+    return 0;
+}
+// Output: 
+// Student name: Abdul subhan
+// Age is: 20
+// Student marks: 89.5
