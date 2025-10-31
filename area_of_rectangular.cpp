b8409717fa5cb69008c9ddfed9c8f22b75e07bd2
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:43:42 +0500

    Add files via upload

---
+++ area_of_rectangular.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 9: area of rectangle
+    int length = 2;
+    int width = 4;
+    int area = length*width;
+    cout<<"Area of rectangle = "<< area;
+    return 0;
+}
+// Area of rectangle = 8
