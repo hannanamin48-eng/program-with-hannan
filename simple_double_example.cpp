09bfec64c96b1edda477bfca15d053853fb9a689
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:44:56 +0500

    Add files via upload

---
+++ simple_double_example.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //10: double simple example
+    double pi = 3.14159;
+    cout<<"The value of pi ="<< pi;
+    return 0;
+}
+// Output
+// The value of pi =3.14159
