05fe0fc2e758083b27d164ca318846d2823d68a1
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:52:14 +0500

    Add user info display program
    This program displays user information including name, roll number, and department.

---
+++ display_user_info.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 13:declaure user info
+    string name = "Sara"; // string declaure to store name Sara 
+    int roll_number = 23; // int used to declaure roll number and assign value 23 
+    string department = "Computer Science"; // string declaure department 
+    cout<< "Name:"<< name<<endl;
+    cout<< "Roll number:" <<  roll_number <<endl;
+    cout<< "Department:" << department;
+    return 0;
+}
+// Output 
+// Name:Sara
+// Roll number:23
+// Department:Computer Science
