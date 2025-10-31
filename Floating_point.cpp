71bd2d4937ede350d5cfc19961d3d3ecbcafe57c
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:16:35 +0500

    Add program to display body temperature

---
+++ Floating_point.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    //5.floating point 
+    float temperature = 36.6;
+    cout << "The body temperature is " << temperature << "°C" << endl;
+    return 0;
+}
+//output
