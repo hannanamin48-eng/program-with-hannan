baae2cd97241b0f507c83c39293e0fd92889cabf
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:08:38 +0500

    Create display_no.cpp
    Implement a simple program to display two integers.

---
+++ display_no.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 3.Display two number
+    int a=10;
+    int b=20;
+    cout<<"The value of a is " << a; 
+    cout<<" and value of b is " << b;
+}
+// Output 
+// The value of a is 10 and value of b is 20
