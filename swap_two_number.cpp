6080bc6e8a50d8a3755984216f154126967da236
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:48:02 +0500

    Add program to swap two numbers
    Implemented a simple program to swap two numbers.

---
+++ swap_two_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //12: Swapping two number (Without logic)
+    int p = 5,q = 10;
+    //before swapping: a = 5 and b =10
+    cout << "Before swapping a = "<< p <<", b = "<< q << endl;
+    // After swapping: a = 10 and b = 5
+    p = 10, q = 5;
+    cout << "After swapping a = "<< p <<", b = "<< q <<endl;
+    return 0;
+} 
+// Output
+// Before swapping a = 5, b = 10
+// After swapping a = 10, b = 5
