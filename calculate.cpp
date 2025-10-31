878af55f6a1194ba463c5ea089f978b92d591750
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:38:51 +0500

    Add files via upload
    .

---
+++ calculate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    //7.calculate and sum and print sum x and y = ? 
+    int x = 15;
+    int y = 25;
+    int sum = x+y;
+    cout<< "Sum of x and y is "<< sum;
+    return 0;
+}
+//output
+// Sum of x and y is 40 
