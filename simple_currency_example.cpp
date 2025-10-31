c34ab7ebe7a408fc4423b2e55008846ec632fd80
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:58:55 +0500

    Add simple currency example in C++

---
+++ simple_currency_example.cpp
@@ -0,0 +1,11 @@
+include<iostream>
+using namespace std;
+int main()
+{  
+    // 14: Simple currency example
+    float price = 250.75;
+    cout<<"Total price:"<< price; 
+    return 0;
+}
+//Output
+// Total price:250.75
