6ee854eb8d0c2e78b7d521602bcad1d6dcf544df
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:05:45 +0500

    Add Print_name.cpp to display a name

---
+++ Print_name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //2.print your name
+    string name = "Hannan Amin";
+    cout<<"My name is "<<name;
+    return 0;
+}
+//Output
+// My name is Hannan Amin
