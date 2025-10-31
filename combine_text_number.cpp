41d99e7ec7afd406b8ad04ea4d26f440c6318de1
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:23:11 +0500

    Create combine_text_number.cpp to display year
    Add a program to combine and display text with an integer.

---
+++ combine_text_number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //6.Combine text and number using integer
+    int a = 2025;
+    cout<<"The current year is "<< a;
+    return 0;
+}
+//output 
+// The current year is 2025
