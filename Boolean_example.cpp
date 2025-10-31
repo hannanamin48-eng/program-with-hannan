ea5ee200f4fde81c6195f4e41d4989fead5242b9
hannanamin48-eng <hannanamin48@gmail.com>
2025-10-31 18:46:47 +0500

    Add boolean example in Boolean_example.cpp
    Demonstrate boolean declaration and output in C++.

---
+++ Boolean_example.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 11:boolean example
+    //how to declaure and print
+    bool israining = false;
+    cout<<"is it raining? "<< israining << endl;
+    //display 0 
+    //display "yes" or "no"
+    cout<<"is it raining?"<< ( israining? "Yes" : "No");
+    return 0;
+}
+// output 
+// is it raining? 0
+//is it raining?No
