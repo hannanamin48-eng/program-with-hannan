40eb1fee220f63f05ecc1bfe01a937e8751f3db3
hannanamin48-eng <hannanamin48@gmail.com>
2025-11-03 21:01:28 +0500

    Datatype.cpp for user input demonstration

---
+++ Datatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age;
+float salary;
+double distance;
+char grade;
+cout<<"enter your name : ";
+cin >>age;
+cout<<"enter your salary :";
+cin >>salary;
+cout<<"enter your distance :";
+cin >>distance;
+cout<<"enter your grade :";
+cin >>grade;
